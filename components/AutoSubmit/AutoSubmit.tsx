4c403c5643e20d70ac34d8b4bc437329d20bf7af
clew <lewis@clewup.co.uk>
2023-05-28 17:23:27 +0100

    feat(component): add autosubmit

---
+++ components/AutoSubmit/AutoSubmit.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useFormikContext } from 'formik'
+import React, { useEffect } from 'react'
+
+// should only be used in forms where text inputs are not being used
+const AutoSubmit = () => {
+    const { values, submitForm } = useFormikContext()
+
+    useEffect(() => {
+        submitForm()
+    }, [values])
+
+    return <></>
+}
+
+export default AutoSubmit
