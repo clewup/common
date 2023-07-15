6d711fe025b623dee030a57100d07849e82aeade
clew <lewis@clewup.co.uk>
2023-07-15 21:59:04 +0100

    feat(auth): add function to fetch client credentials access token

--- hooks/useAuth/useAuth.ts
+++ hooks/useAuth/useAuth.ts
@@ -1,95 +1,109 @@
-'use client'
+'use client';
 
-import { LockrContext } from '@/lib/common/contexts/LockrContext/LockrContext'
-import { type UserType } from '@/lib/common/types/userTypes'
-import jwt from 'jsonwebtoken'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useContext, useEffect, useState } from 'react'
+import { LockrContext } from '@/lib/common/contexts/LockrContext/LockrContext';
+import { type UserType } from '@/lib/common/types/userTypes';
+import { GrantTypes } from '@/lib/common/types/tokenTypes';
+import jwt from 'jsonwebtoken';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useContext, useEffect, useState } from 'react';
 
 interface UseAuthProps {
-    redirectUri?: string
-    applicationId?: number
+    redirectUri?: string;
+    applicationId?: number;
 }
 
 const useAuth = ({ applicationId, redirectUri }: UseAuthProps) => {
-    const context = useContext(LockrContext)
-    const searchParams = useSearchParams()
-    const pathname = usePathname()
-    const code = searchParams.get('code')
+    const context = useContext(LockrContext);
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const code = searchParams.get('code');
 
-    const [isAuthing, setAuthing] = useState(false)
+    const [isAuthing, setAuthing] = useState(false);
 
     if (!context) {
-        throw new Error('useAuth may only be used within the LockrContext')
+        throw new Error('useAuth may only be used within the LockrContext');
     }
-    const { setAdmin, setUser, user } = context
+    const { setAdmin, setUser, user } = context;
 
-    const router = useRouter()
+    const router = useRouter();
 
-    const LOCKR_BASE_URL = process.env.LOCKR_BASE_URL ?? 'https://lockr.clewup.co.uk'
+    const LOCKR_BASE_URL = process.env.LOCKR_BASE_URL ?? 'https://lockr.clewup.co.uk';
     const REDIRECT_URI =
         redirectUri ??
         process.env.NEXT_PUBLIC_LOCKR_APP_URL ??
         process.env.LOCKR_APP_URL ??
         process.env.NEXT_PUBLIC_APP_URL ??
-        (process.env.APP_URL as string)
+        (process.env.APP_URL as string);
     const APPLICATION_ID =
-        applicationId ?? process.env.NEXT_PUBLIC_LOCKR_APPLICATION_ID ?? (process.env.LOCKR_APPLICATION_ID as string)
+        applicationId ?? process.env.NEXT_PUBLIC_LOCKR_APPLICATION_ID ?? (process.env.LOCKR_APPLICATION_ID as string);
+    const APPLCIATION_SECRET =
+        process.env.NEXT_PUBLIC_LOCKR_APPLICATION_SECRET ?? (process.env.LOCKR_APPLICATION_SECRET as string);
 
     function signIn() {
-        router.push(`${LOCKR_BASE_URL}?redirect_uri=${REDIRECT_URI}&application_id=${APPLICATION_ID}`)
+        router.push(`${LOCKR_BASE_URL}?redirect_uri=${REDIRECT_URI}&application_id=${APPLICATION_ID}`);
     }
 
     function signOut() {
-        localStorage.removeItem('lockr.at')
-        setUser(null)
-        setAdmin(false)
-        router.push('/')
+        localStorage.removeItem('lockr.at');
+        setUser(null);
+        setAdmin(false);
+        router.push('/');
     }
 
     async function fetchAccessToken(code: string): Promise<string> {
         const accessTokenResponse = await fetch(`${LOCKR_BASE_URL}/api/auth/token`, {
-            body: JSON.stringify({ authorization_code: code }),
+            body: JSON.stringify({ grant_type: GrantTypes.AuthorizationCode, authorization_code: code }),
             method: 'POST',
-        })
+        });
 
-        if (!accessTokenResponse.ok) throw new Error('An error occurred fetching the access token')
+        if (!accessTokenResponse.ok) throw new Error('An error occurred fetching the access token');
 
-        const accessTokenData = await accessTokenResponse.json()
-        return accessTokenData.access_token
+        const accessTokenData = await accessTokenResponse.json();
+        return accessTokenData.access_token;
+    }
+
+    async function fetchClientAccessToken() {
+        const accessTokenResponse = await fetch(`${LOCKR_BASE_URL}/api/auth/token`, {
+            body: JSON.stringify({
+                grant_type: GrantTypes.ClientCredentials,
+                client_id: APPLICATION_ID,
+                client_secret: APPLCIATION_SECRET,
+            }),
+            method: 'POST',
+        });
     }
 
     async function handleAuthorization(code: string) {
-        const accessToken = await fetchAccessToken(code)
-        localStorage.setItem('lockr.at', accessToken)
+        const accessToken = await fetchAccessToken(code);
+        localStorage.setItem('lockr.at', accessToken);
 
-        const decodedToken = jwt.decode(accessToken) as UserType
-        setUser(decodedToken)
-        setAdmin(decodedToken.role === 'Admin')
+        const decodedToken = jwt.decode(accessToken) as UserType;
+        setUser(decodedToken);
+        setAdmin(decodedToken.role === 'Admin');
 
-        router.push(pathname)
+        router.push(pathname);
     }
 
     useEffect(() => {
-        const accessToken = localStorage.getItem('lockr.at')
+        const accessToken = localStorage.getItem('lockr.at');
         if (accessToken) {
-            const decodedToken = jwt.decode(accessToken) as UserType
-            setUser(decodedToken)
-            setAdmin(decodedToken.role === 'Admin')
+            const decodedToken = jwt.decode(accessToken) as UserType;
+            setUser(decodedToken);
+            setAdmin(decodedToken.role === 'Admin');
         }
 
         if (code && user == null && !isAuthing) {
-            setAuthing(true)
-            handleAuthorization(code)
-            setAuthing(false)
+            setAuthing(true);
+            handleAuthorization(code);
+            setAuthing(false);
         }
-    }, [code])
+    }, [code]);
 
     return {
         isAuthing,
         signIn,
         signOut,
-    }
-}
+    };
+};
 
-export default useAuth
+export default useAuth;
