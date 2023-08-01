b4a67d36826226c54eeb65d5845aba00ef55a7b5
clew <lewis@clewup.co.uk>
2023-08-01 22:17:56 +0100

    feat(cookie): remove daisyui dependency

--- components/CookiePopup/CookiePopup.tsx
+++ components/CookiePopup/CookiePopup.tsx
@@ -23,33 +23,28 @@ const CookiePopup: FC<CookiePopupProps> = ({ customMessage }) => {
         setRendered(false)
     }
 
+   if (!isRendered) return null
+
     return (
-        <div>
-            <input type="checkbox" id="my-modal" className="modal-toggle" checked={isRendered} onChange={() => null} />
-            <div className="modal">
-                <div className="modal-box">
-                    <h3 className="font-bold text-2xl text-center">We value your privacy! 🍪</h3>
-                    <p className="pt-4 text-center">
-                        We use cookies to enhance your browsing experience and to improve our service. By clicking
-                        &apos;Accept All&apos;, you consent to our use of cookies.
-                    </p>
-                    <div className="modal-action justify-center gap-10">
-                        <button
-                            className="btn btn-ghost"
-                            onClick={async () => {
-                                await updateCookieConsent(false)
-                            }}>
-                            Reject All
-                        </button>
-                        <button
-                            className="btn btn-success"
-                            onClick={async () => {
-                                await updateCookieConsent(true)
-                            }}>
-                            Accept All
-                        </button>
-                    </div>
-                </div>
+        <div className="rounded-md p-5 bg-white">
+            <h3 className="font-bold text-2xl text-center">We value your privacy! 🍪</h3>
+            <p className="pt-4 text-center">
+                We use cookies to enhance your browsing experience and to improve our service. By clicking
+                &apos;Accept All&apos;, you consent to our use of cookies.
+            </p>
+            <div className="flex mt-10 justify-center gap-10">
+                <button
+                    onClick={async () => {
+                        await updateCookieConsent(false)
+                    }}>
+                    Reject All
+                </button>
+                <button
+                    onClick={async () => {
+                        await updateCookieConsent(true)
+                    }}>
+                    Accept All
+                </button>
             </div>
         </div>
     )
---
+++ contexts/CookieContext/CookieContext.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import CookiePopup from "@/lib/common/components/CookiePopup/CookiePopup";
+import React, {createContext, FC, ReactNode} from "react";
+
+const CookieContext = createContext({})
+
+interface CookieProviderProps {
+    children: ReactNode;
+}
+
+const CookieProvider: FC<CookieProviderProps> = ({children}) => {
+    return (
+        <CookieContext.Provider value={{}}>
+            <div>
+                <div className="fixed bottom-5 z-50 right-5 w-1/5">
+                    <CookiePopup/>
+                </div>
+
+                {children}
+            </div>
+        </CookieContext.Provider>
+    )
+}
+
+export default CookieProvider;
